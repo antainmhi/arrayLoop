1b87de9588f4fbc8e5f4afb359b35547b4a0c6f3
antainmhi <52220856+antainmhi@users.noreply.github.com>
2021-08-03 18:16:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//create function
+function go(){
+
+    //create array
+    var arr = [2, 5, 7, 9, 103, 88, 3, 77, 46, 1002, 798, 534, 88]; 
+    var largest = arr[0];
+   
+    
+    //for loop through array to find largest number
+    for (i = 0; i < arr.length; i++){
+        if (largest < arr[i]){
+            largest = arr[i];
+
+            
+        }
+        
+            
+    }
+    //print largest number
+    document.getElementById("main").innerHTML+=largest;
+    
+
+
+
+}
